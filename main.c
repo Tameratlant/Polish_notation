5bf28fee7498a6f3f96a318d630ce821cd4c96a4
Tameratlant <80047396+Tameratlant@users.noreply.github.com>
2022-08-21 19:31:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,28 +5,46 @@
 int main () {
     char * s;
     char buf[256];
-	double result;
+	// double result;
     s = calloc(256, sizeof(char));
+
+    char* infixExpr = calloc(256, sizeof(char));
+
+    char* withoutx = calloc(256, sizeof(char));
+
     fgets(buf, 256, stdin);
-    s = ToPostfix(buf);
+
+    infixExpr = no_sin(buf);
+
+    printf("With x\n");
+
+    printf("\n%s\n", infixExpr);
+
+    s = ToPostfix(infixExpr);
+
+    printf("With x\n");
+
     printf("\n%s\n", s);
+
+    withoutx = convert_x(s, 1.5);
+
+    printf("Without x\n");
+
+    printf("\n%s\n", withoutx);
+
+    // printf("\n%s\n", withoutx);
     
-	eval_rpn(&result, s);
-	printf("result = %f\n", result);
+
+
+	// eval_rpn(&result, s);
+	// printf("result = %f\n", result);
     //printf("%s", GetStringNumber("1.5123", &i));
     // free(ToPostfix);
     // free(s);
     return 0;
-}
 
-// char *pc;
-// 	char buf[256];
-// 	double result;
-	
-// 	while(1)
-// 	{
-// 		printf("Enter Expression. Example: 2 3 4 + *(= (3+4)*2). ctrl + C to exit:\n");
-// 		fgets(buf, 256, stdin);
-// 		eval_rpn(&result, buf);
-// 		printf("result = %f\n", result);
-// 	}
+
+
+
+
+}
