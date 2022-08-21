6dbd071bec7b553a27ed68e115482c87de7c2fb2
Tameratlant <80047396+Tameratlant@users.noreply.github.com>
2022-08-21 19:31:42 +0300

    Update convert.c

--- convert.c
+++ convert.c
@@ -1,6 +1,5 @@
 #include "convert.h"
 
-
 // typedef struct elem{
 //     char* sign;
 //     double num;
@@ -17,13 +16,14 @@
 // // }
 
 int check_digit(char c) {
-    int flag = 0;
-    if (c < 58 && c > 47) flag = 1;
-    return flag; 
+  int flag = 0;
+  if (c < 58 && c > 47)
+    flag = 1;
+  return flag;
 }
 
 int operationPriority(char c) {
-    int flag = -1;
+  int flag = -1;
     if (c == '(') flag = 0;
     if (c == '+') flag = 1;
     if (c == '-') flag = 1;
@@ -35,7 +35,7 @@ int operationPriority(char c) {
     if (c == 't') flag = 4;
     if (c == 'T') flag = 4;
     if (c == '~') flag = 4;
-    return flag;
+  return flag;
 }
 
 // void prepare(char* in) {
@@ -49,7 +49,7 @@ int operationPriority(char c) {
 // }
 
 // // void convert(char* in, char* out) {
-    
+
 // // }
 // //48 - 57 - числа
 // int main() {
@@ -57,73 +57,62 @@ int operationPriority(char c) {
 //     printf("%d", a);
 // }
 
-int check_sin(char *str, int pos){
-      int i = pos;
-      if (str[i] == 's' && str[i+1] == 'i' && str[i+2] == 'n'){
-        return 1;
-      }
-      else 
-{
-  return 0;
+int check_sin(char *str, int pos) {
+  int i = pos;
+  if (str[i] == 's' && str[i + 1] == 'i' && str[i + 2] == 'n') {
+    return 1;
+  } else {
+    return 0;
+  }
 }
-    }
-    int check_cos(char *str, int pos){
-      int i = pos;
-      if (str[i] == 'c' && str[i+1] == 'o' && str[i+2] == 's'){
-        return 1;
-      }
-      else 
-{
-  return 0;
+int check_cos(char *str, int pos) {
+  int i = pos;
+  if (str[i] == 'c' && str[i + 1] == 'o' && str[i + 2] == 's') {
+    return 1;
+  } else {
+    return 0;
+  }
 }
-    }
-    int check_tan(char *str, int pos){
-      int i = pos;
-      if (str[i] == 't' && str[i+1] == 'a' && str[i+2] == 'n'){
-        return 1;
-      }
-      else 
-{
-  return 0;
+int check_tan(char *str, int pos) {
+  int i = pos;
+  if (str[i] == 't' && str[i + 1] == 'a' && str[i + 2] == 'n') {
+    return 1;
+  } else {
+    return 0;
+  }
 }
-    }
-    int check_ctg(char *str, int pos){
-      int i = pos;
-      if (str[i] == 'c' && str[i+1] == 't' && str[i+2] == 'g'){
-        return 1;
-      }
-      else 
-{
-  return 0;
+int check_ctg(char *str, int pos) {
+  int i = pos;
+  if (str[i] == 'c' && str[i + 1] == 't' && str[i + 2] == 'g') {
+    return 1;
+  } else {
+    return 0;
+  }
 }
-    }
 
-
-char* GetStringNumber(char* expr, int* pos) {
-  //printf("\nPos %d\n", *pos);
+char *GetStringNumber(char *expr, int *pos) {
+  // printf("\nPos %d\n", *pos);
   //	Хранит число
-//   char strNumber[100] = "";
-  char* strNumber = calloc(100, sizeof(char));
+  //   char strNumber[100] = "";
+  char *strNumber = calloc(100, sizeof(char));
   int len = strlen(expr);
   //	Перебираем строку
   int i = *pos;
   for (; i < len; i++) {
-    //printf("\nPos = %d\n", i);
+    // printf("\nPos = %d\n", i);
     //	Разбираемый символ строки
     char num = expr[i];
-	
+
     //	Проверяем, является символ числом
-    
     if (check_digit(num) || num == '.' || num == 'x') {
+      printf("!!!");
       //	Если да - прибавляем к строке
       char s[2];
       s[1] = '\0';
       s[0] = num;
       strcat(strNumber, s);
-    //   strNumber += num;
-    }
-    else
-    {
+      //   strNumber += num;
+    } else {
       //	Если нет, то перемещаем счётчик к предыдущему символу
       i--;
       //	И выходим из цикла
@@ -132,14 +121,119 @@ char* GetStringNumber(char* expr, int* pos) {
   }
 
   //	Возвращаем число
-  //printf("Pos = %d\n", i);
+  // printf("Pos = %d\n", i);
   *pos = i;
   return strNumber;
 }
 
+char *ToPostfix(char *infixExpr) {
+  //	Выходная строка, содержащая постфиксную запись
+  int len = strlen(infixExpr);
+
+  // printf("\n%s\n", infixExpr);
+
+  char *postfixExpr = calloc(22 * len, sizeof(char));
+  //	Инициализация стека, содержащий операторы в виде символов
+
+  stack *st = init();
+
+  //	Перебираем строку
+  for (int i = 0; i < len; i++) {
+    //	Текущий символ
+    // printf("\n%c\n", infixExpr[i]);
+    char c = infixExpr[i];
+
+    //	Если симовол - цифра
+    if (check_digit(c) || c == 'x') {
+      //	Парсии его, передав строку и текущую позицию, и заносим в
+      //выходную строку printf("\n%c\n", c);
+      postfixExpr = realloc(postfixExpr, sizeof(char));
+      //printf("i = %d", i);
+      strcat(postfixExpr, GetStringNumber(infixExpr, &i));
+      strcat(postfixExpr, " ");
+    }
+    //	Если открывающаяся скобка
+    else if (c == '(') {
+      //	Заносим её в стек
+      push(st, c);
+    }
+    //	Если закрывающая скобка
+    else if (c == ')') {
+      //	Заносим в выходную строку из стека всё вплоть до открывающей
+      //скобки
+      // while (stack.Count > 0 && stack.Peek() != '(')
+      while (size(st) > 0 && peek(st) != '(') {
+        char s[2];
+        s[1] = '\0';
+        s[0] = pop(st);
+        printf("\nZnak = %s\n", s);
+        strcat(postfixExpr, s);
+        strcat(postfixExpr, " ");
+      }
+      //	Удаляем открывающуюся скобку из стека
+      pop(st);
+    }
+    //Проверяем, содержится ли символ в списке операторов
+
+    else if (operationPriority(c) != -1) {
+      printf("\nznak = %c %d\n", c, operationPriority(c));
+      //	Если да, то сначала проверяем
+      char op = c;
+      //	Является ли оператор унарным символом
+      if (op == '-' &&
+          (i == 0 || (i > 1 && operationPriority(infixExpr[i - 1]) != -1)))
+        //	Если да - преобразуем его в тильду
+        op = '~';
+      // printf("%c", op);
+      // push(st, op);
+      //	Заносим в выходную строку все операторы из стека, имеющие более
+      //высокий приоритет
+      // while (stack.Count > 0 && ( operationPriority[stack.Peek()] >=
+      // operationPriority[op]))
+
+      // peek(st);
+      // printf("!%c!", peek(st));
+      while (size(st) > 0 &&
+             (operationPriority(peek(st)) >= operationPriority(op))) {
+        printf("Current stack:");
+        print_list(st->pointer);
+        postfixExpr = realloc(postfixExpr, sizeof(char));
+        char s[2];
+        s[1] = '\0';
+        s[0] = pop(st);
+        printf("\nZnak = %s\n", s);
+        strcat(postfixExpr, s);
+        strcat(postfixExpr, " ");
+        // strcat(postfixExpr, pop(st));
+      }
+
+      // postfixExpr += stack.Pop();
+      //	Заносим в стек оператор
+      // stack.Push(op);
+      push(st, op);
+      // printf("!%c!", peek(st));
+    }
+  }
+  //	Заносим все оставшиеся операторы из стека в выходную строку
+  list *p = st->pointer;
+  while (p != NULL) {
+    char s[2];
+    s[1] = '\0';
+    s[0] = pop(st);
+    printf("\nZZnak = %c\n", s[0]);
+    // s1[0] = s[1];
+    strcat(postfixExpr, s);
+    strcat(postfixExpr, " ");
+    p = p->next;
+  }
+
+  //	Возвращаем выражение в постфиксной записи
+  return postfixExpr;
+}
+
 char* no_sin(char * in) {
   int len = strlen(in);
-  char* infixExpr = calloc(len, sizeof(char));
+  char* infixExpr = calloc(2 * len, sizeof(char));
   for (int i = 0; i < len ; i++) {
     if (check_sin(in, i) == 1) {
           i = i + 2;
@@ -147,147 +241,56 @@ char* no_sin(char * in) {
           s[1] = '\0';
           s[0] = 's';
           strcat(infixExpr, s);
-          strcat(infixExpr, " ");
+          // strcat(infixExpr, " ");
         } else if (check_cos(in, i) == 1) {
           i = i + 2;
           char s[2];
           s[1] = '\0';
           s[0] = 'c';
           strcat(infixExpr, s);
-          strcat(infixExpr, " ");
+          // strcat(infixExpr, " ");
         } else if (check_tan(in, i) == 1) {
           i = i + 2;
           char s[2];
           s[1] = '\0';
           s[0] = 't';
           strcat(infixExpr, s);
-          strcat(infixExpr, " ");
+          // strcat(infixExpr, " ");
         } else if (check_ctg(in, i) == 1) {
           i = i + 2;
           char s[2];
           s[1] = '\0';
           s[0] = 'T';
           strcat(infixExpr, s);
-          strcat(infixExpr, " ");
+          // strcat(infixExpr, " ");
         } else {
           char s[2];
           s[1] = '\0';
           s[0] = in[i];
           strcat(infixExpr, s);
-          strcat(infixExpr, " ");
+          // strcat(infixExpr, " ");
         }
   }
   return infixExpr;
 }
 
-char* ToPostfix(char* infixExpr) {
-    //	Выходная строка, содержащая постфиксную запись
-    int len = strlen(infixExpr);
+char* convert_x(char* str, double x) {
+  int len = strlen(str);
+  char* res = calloc(10 * len, sizeof(char));
 
-    //printf("\n%s\n", infixExpr);
-
-    char* postfixExpr = calloc(22 * len, sizeof(char));
-    //	Инициализация стека, содержащий операторы в виде символов
-
-    stack* st = init();
-
-		//	Перебираем строку
-    for (int i = 0; i < len; i++) {
-      //	Текущий символ
-      //printf("\n%c\n", infixExpr[i]);
-      char c = infixExpr[i];
-      
-      //	Если симовол - цифра
-      if (check_digit(c)) {
-         //	Парсии его, передав строку и текущую позицию, и заносим в выходную строку
-         //printf("\n%c\n", c);
-         postfixExpr = realloc (postfixExpr, sizeof(char));
-         printf("i = %d", i);
-         strcat(postfixExpr, GetStringNumber(infixExpr, &i));
-         strcat(postfixExpr, " ");
-      }
-      //	Если открывающаяся скобка 
-      else if (c == '(')
-      {
-        	//	Заносим её в стек
-        	push(st, c);
-      }
-      //	Если закрывающая скобка
-      else if (c == ')')
-      {
-        	//	Заносим в выходную строку из стека всё вплоть до открывающей скобки
-          //while (stack.Count > 0 && stack.Peek() != '(')
-          while (size(st) > 0 && peek(st) != '(') {
-              char s[2];
-              s[1] = '\0';
-            s[0] = pop(st);
-            printf("\nZnak = %s\n", s);
-            strcat(postfixExpr, s);
-            strcat(postfixExpr, " ");
-          }
-        	//	Удаляем открывающуюся скобку из стека
-          pop(st);
-      }
-      	//Проверяем, содержится ли символ в списке операторов
-        
-      else if (operationPriority(c) != -1)
-      {
-        printf("\nznak = %c %d\n", c,operationPriority(c));
-        //	Если да, то сначала проверяем
-        char op = c;
-        //	Является ли оператор унарным символом
-        if (op == '-' && (i == 0 || (i > 1 && operationPriority( infixExpr[i-1] ) != -1)))
-          //	Если да - преобразуем его в тильду
-          op = '~';
-          //printf("%c", op);
-				//push(st, op);
-        //	Заносим в выходную строку все операторы из стека, имеющие более высокий приоритет
-        // while (stack.Count > 0 && ( operationPriority[stack.Peek()] >= operationPriority[op]))
-
-        // peek(st);
-        //printf("!%c!", peek(st));
-        while (size(st) > 0 && ( operationPriority(peek(st)) >= operationPriority(op))) {
-          printf("Current stack:");
-          print_list(st->pointer);
-          postfixExpr = realloc (postfixExpr, sizeof(char));
-            char s[2];
-            s[1] = '\0';
-            s[0] = pop(st);
-            printf("\nZnak = %s\n", s);
-            strcat(postfixExpr, s);
-            strcat(postfixExpr, " ");
-          //strcat(postfixExpr, pop(st));
-        }
-                
-        		// postfixExpr += stack.Pop();
-				//	Заносим в стек оператор
-        // stack.Push(op);
-        push(st, op);
-        //printf("!%c!", peek(st));
-      }
+  for (int i = 0; i < len; i++) {
+    if (str[i] == 'x') {
+    char buff[20];
+    sprintf(buff, "%.3f", x);
+    // printf("\n%s\n", buff);
+      strcat(res, buff);
+      // strcat(res, " ");
+    } else {
+      char s[2];
+      s[1] = '\0';
+      s[0] = str[i];
+      strcat(res, s);
     }
-  	//	Заносим все оставшиеся операторы из стека в выходную строку
-        list *p = st -> pointer;
-        while (p != NULL) {
-          char s[2];
-          s[1] = '\0';
-            s[0] = pop(st);
-            printf("\nZZnak = %c\n", s[0]);
-            // s1[0] = s[1];
-            strcat(postfixExpr, s);
-            strcat(postfixExpr, " ");
-            p = p->next;
-        }
-
-  	//	Возвращаем выражение в постфиксной записи
-    return postfixExpr;
+  }
+  return res;
 }
-
-// char* double_to_string(double x) {
-
-// }
-
-// char* void convert_x(char* s, double x) {
-
-// }
-
